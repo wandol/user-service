89680c020ab0789c7387b14c1d8654fc4317e8ec
wandol <wandol@kakao.com>
2021-07-11 23:17:08 +0900

    user service all,byuserid 추가.

--- src/main/java/com/example/userservice/controller/UserController.java
+++ src/main/java/com/example/userservice/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.userservice.controller;
 
 import com.example.userservice.dto.UserDto;
+import com.example.userservice.jpa.UserEntity;
 import com.example.userservice.service.UserService;
 import com.example.userservice.vo.Greeting;
 import com.example.userservice.vo.RequestUser;
@@ -12,6 +13,10 @@ import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.lang.annotation.Repeatable;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/user-service")
@@ -50,5 +55,22 @@ public class UserController {
         ResponseUser responseUser = modelMapper.map(userDto,ResponseUser.class);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(responseUser);
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<ResponseUser>> getUsers(){
+        Iterable<UserEntity> userList = userService.getUserByAll();
+        List<ResponseUser> result = new ArrayList<>();
+        userList.forEach(v -> {
+            result.add(new ModelMapper().map(v,ResponseUser.class));
+        });
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    @GetMapping("/users/{userId}")
+    public ResponseEntity<ResponseUser> getUser(@PathVariable("userId") String userId){
+        UserDto user = userService.getUserByUserId(userId);
+        ResponseUser responseUser = new ModelMapper().map(user,ResponseUser.class);
+        return ResponseEntity.status(HttpStatus.OK).body(responseUser);
     }
 }
--- src/main/java/com/example/userservice/jpa/UserRepository.java
+++ src/main/java/com/example/userservice/jpa/UserRepository.java
@@ -3,4 +3,5 @@ package com.example.userservice.jpa;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<UserEntity,Long> {
+    UserEntity findAllByUserId(String userId);
 }
